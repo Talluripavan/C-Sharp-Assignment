189c0adc914b22e59c0d1de7dde4ed2583b97d3a
Talluri Pavan <46924038+Talluripavan@users.noreply.github.com>
2022-02-15 18:43:18 +0530

    Add files via upload
    Tick Toc Toe Assignment

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Your age:");
+    A:
+        string line = Console.ReadLine();
+        try
+        {
+            age = Int32.Parse(line);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("{0} is not an integer", line);
+            // Return? Loop round? Whatever.
+            goto A;
+        }
+    }
+}
